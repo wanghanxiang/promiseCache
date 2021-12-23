4fb7e12984c451155ef1874551102d75530a70d0
hanxiang1 <hanxiang1@staff.sina.com.cn>
2021-12-23 17:11:27 +0800

    fix:执行报错

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import cache from './utils/promiseCache';
 
-function getName(): Promise<string> {
-  return new Promise(async (resolve, reject) => {
-    await new Promise(res => setTimeout(res, 3000));
-    return resolve("{123}");
-  });
+function getName(name: string):Promise<any> {
+    return new Promise(async (resolve, reject) => {
+        await new Promise(res => setTimeout(res, 3000));
+        return resolve(name);
+    });
 }
 
 //执行python代码块
 (async () => {
-  const res = await cache.get<string>('get-user', getName) // 如多次发起请求，只会请求一次
-  console.info(res)
-  const res2 = await cache.get('get-user', getName) // 如多次发起请求，只会请求一次
-  console.info(res2)
+    const res = await cache.get('get-user', getName("123")) // 如多次发起请求，只会请求一次
+    console.info(res)
+    const res2 = await cache.get('get-user', getName("456")) // 如多次发起请求，只会请求一次
+    console.info(res2)
 })();
--- utils/promiseCache.ts
+++ utils/promiseCache.ts
@@ -5,13 +5,13 @@ import NodeCache = require('node-cache');
  * @description 处理重复的promise对象请求
  */
 interface Task<T = any> {
-  resolve: (res: T) => void
-  reject: (err: any) => void
+    resolve: (res: T) => void
+    reject: (err: any) => void
 }
 
 const options = {
-  maxKeys: 1000,
-  stdTTL: 1000 * 60 * 60 * 1 //一小时
+    maxKeys: 1000,
+    stdTTL: 1000 * 60 * 60 * 1 //一小时
 };
 /**
  * 
@@ -22,80 +22,80 @@ const options = {
  * 
  */
 class CachePromise {
-  private pendingTask: { [id: string]: Task[] } = {};
-  private cache = new NodeCache(options);
-  private isCache = false; //是否开启缓存功能
+    private pendingTask: { [id: string]: Task[] } = {};
+    private cache = new NodeCache(options);
+    private isCache = false; //是否开启缓存功能
 
-  constructor(isCache: boolean) {
-    this.isCache = isCache;
-  }
-
-  /**
-   * @param id 唯一键
-   * @param promiseFun 异步方法
-   * @param ttl 缓存老化时间 分钟
-   * @param isRefresh 是否刷新缓存结果.true 则表示本次不使用缓存
-   * @returns 
-   */
-  public async get<T>(id: string, promiseFun: () => Promise<T>, ttl: number = 30, isRefresh: boolean = false) {
-    if (id in this.pendingTask) {
-      return new Promise((resolve:any, reject:any) => {
-        this.pendingTask[id].push({ resolve, reject })
-      })
+    constructor(isCache: boolean) {
+        this.isCache = isCache;
     }
 
-    if (this.isCache && !isRefresh && this.cache.get(id)) { //如果开启缓存，缓存里面有就返回缓存里面的
-      return this.cache.get(id);
+    /**
+     * @param id 唯一键
+     * @param promiseFun 异步方法
+     * @param ttl 缓存老化时间 分钟
+     * @param isRefresh 是否刷新缓存结果.true 则表示本次不使用缓存
+     * @returns 
+     */
+    public async get(id: string, promiseFun: any, ttl: number = 30, isRefresh: boolean = false) {
+        if (id in this.pendingTask) {
+            return new Promise((resolve: any, reject: any) => {
+                this.pendingTask[id].push({ resolve, reject })
+            })
+        }
+    
+        if (this.isCache && !isRefresh && this.cache.get(id)) { //如果开启缓存，缓存里面有就返回缓存里面的
+            return this.cache.get(id);
+        }
+
+        let res: any | undefined
+        let err: any
+        try {
+            this.pendingTask[id] = [];
+            res = await promiseFun;
+            this.isCache && this.setCache(id, res, ttl);
+        } catch (err) {
+            err = err
+        }
+
+        for (let t of this.pendingTask[id]) {
+            if (err != null) {
+                t.reject(err)
+            } else {
+                t.resolve(res)
+            }
+        }
+
+        delete this.pendingTask[id];
+
+        if (err != null) {
+            throw err;
+        }
+
+        return res as any;
     }
 
-    let res: T | undefined
-    let err: any
-    try {
-      this.pendingTask[id] = [];
-      res = await promiseFun();
-      this.isCache && this.setCache(id, res,ttl);
-    } catch (err) {
-      err = err
+    /**
+     * @description 更新缓存
+     * @param id 
+     * @param result 
+     * @param ttl 
+     * @returns 
+     */
+    private async setCache(id: string, result: any, ttl: number = 30) {
+        const TTL_MINUTE = 60; //1分钟
+        this.cache.set(id, result, TTL_MINUTE * ttl);
+        return result;
     }
 
-    for (let t of this.pendingTask[id]) {
-      if (err != null) {
-        t.reject(err)
-      } else {
-        t.resolve(res)
-      }
+    /**
+     * 强制刷新全部缓存
+     * @returns 
+     */
+    public flushCache() {
+        this.cache.flushAll();
+        return "success";
     }
-
-    delete this.pendingTask[id];
-
-    if (err != null) {
-      throw err;
-    }
-
-    return res as T;
-  }
-
-  /**
-   * @description 更新缓存
-   * @param id 
-   * @param result 
-   * @param ttl 
-   * @returns 
-   */
-  private async setCache(id: string, result: any, ttl: number = 30) {
-    const TTL_MINUTE = 60; //1分钟
-    this.cache.set(id, result, TTL_MINUTE * ttl);
-    return result;
-  }
-
-  /**
-   * 强制刷新全部缓存
-   * @returns 
-   */
-  public flushCache() {
-    this.cache.flushAll();
-    return "success";
-  }
 
 }
 
